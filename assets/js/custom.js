3a86050fe3f4585c00b31b934bc037a172fda956
Md.Moinul hasan Raju <84829433+Raju-cs@users.noreply.github.com>
2022-07-17 18:18:11 +0600

    rgb

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -107,6 +107,27 @@ window.addEventListener('scroll', function(){
 });
 
 
+// back to top btn
+var t1 = 0;
+window.onscroll = scroll1;
+
+function scroll1() {
+  var toTop = document.getElementById('toTop');
+  window.scrollY > 45 ? toTop.style.display = 'Block' : toTop.style.display = 'none';
+}
+
+function abcd() {
+  var y1 = window.scrollY;
+  y1 = y1 - 1000;
+  window.scrollTo(0, y1);
+  if (y1 > 0) {
+    t1 = setTimeout("abcd()", 50);
+  } else {
+    clearTimeout(t1);
+  }
+}
+
+
 
 
 
